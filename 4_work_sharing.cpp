c303d1d7ab70ba8e6d9faabe7aafa3e30c2362ab
aydinke16@itu.edu.tr <aydinke16@itu.edu.tr>
2023-04-23 12:15:09 +0300

    fixed work sharing with vector

--- 4_work_sharing.cpp
+++ 4_work_sharing.cpp
@@ -1,40 +1,49 @@
 #include <omp.h>
 #include <cstdio>
 #include <chrono>
-#include <ostream>
 #include <iostream>
+#include <vector>
 
-int MAX = 10000000;
+int MAX = 100000;
 
 int main()
 {
 
   double start_time = omp_get_wtime();
 
-  double res[MAX],  kes[MAX], result[MAX];
-  int i;
+  std::vector<double> res(MAX),  kes(MAX), result(MAX);
 
 #pragma omp parallel for
-  for(i=0;i<MAX;i++){
+  for(int i=0;i<MAX;i++){
     res[i] = 10;
-  }
-
-#pragma omp parallel for
-  for(i=0;i<MAX;i++){
     kes[i] = 10;
-  }
-
-	
-#pragma omp parallel for
-  for(i=0;i<MAX;i++){
     result[i] = res[i] + kes[i];
   }
 
 
-	
   double end_time = omp_get_wtime();
 
-  printf("Elapsed time: %f", end_time - start_time);
+  printf("Elapsed time with openmp: %f\n", end_time - start_time);
+  
+  
+  double start_time_2 = omp_get_wtime();
+
+  std::vector<double> res_2(MAX),  kes_2(MAX), result_2(MAX);
+
+
+  for(int i=0;i<MAX;i++){
+    res_2[i] = 10;
+    kes_2[i] = 10;
+    result_2[i] = res_2[i] + kes_2[i];
+  }
+
+
+  double end_time_2 = omp_get_wtime();
+
+  printf("Elapsed time without openmp: %f\n", end_time_2 - start_time_2);
+
+
+
 
 
   return 0;
